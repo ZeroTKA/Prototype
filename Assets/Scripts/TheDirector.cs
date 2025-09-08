00e6da343b4094af2830f8b46f51248e5a906086
ZeroTKA <zerotka@gmail.com>
2025-09-07 19:05:43 -0600

    I think GameState is in???
    At least the makings of the game state machine. We'll see what I need to change later I suppose.

---
+++ Assets/Scripts/TheDirector.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class TheDirector : MonoBehaviour
+{
+    public static TheDirector Instance { get; private set; }
+    public GameState CurrentState { get; private set; }
+    public event Action<GameState> OnGameStateChanged;
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log($"We are here");
+        SetGameState(GameState.MainMenu);
+    }
+
+    public enum GameState
+    {
+        MainMenu,
+        Wave,
+        Shop,
+        GameOver
+    }
+
+    public void SetGameState(GameState newState)
+    {
+        Debug.Log($"{CurrentState} to {newState}");
+        if(CurrentState != newState)
+        {
+            CurrentState = newState;
+            OnGameStateChanged?.Invoke(newState);
+            Debug.Log($"{newState} state activated.");
+        }
+    }
+}
