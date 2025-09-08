c287fd8dc0e4ea7816ace7be0fe891af895a948c
ZeroTKA <zerotka@gmail.com>
2025-09-08 12:44:00 -0600

    SpawnManager Tweaks
    adding SpawnWave method. The idea is it'll be super easy to create waves and go through a switch statement to start the wave we are on.... In theory easy peasy. I suspect SpawnWave() will be further tweaked but I probably shouldn't worry tooooooo much about it for a prototype.

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,19 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
     [SerializeField]GameObject enemyPrefab;    
-    int maxEnemyCount = 4;
     float xMinRange = 1f;
     float xMaxRange = 12f;
     float zMinRange = -30f;
     float zMaxRange = -25f;
     void Start()
     {
-        for(int i = 0; i < maxEnemyCount; i++)
-        {
-            Instantiate(enemyPrefab, RandomPosition(), Quaternion.identity);
-        }
+        StartCoroutine(SpawnWave(enemyPrefab, 10, .5f));
     }
 
     // Update is called once per frame
@@ -28,5 +25,28 @@ public class WaveManager : MonoBehaviour
         float z = Random.Range(zMinRange, zMaxRange);
 
         return new Vector3(x, 1, z);
+    }
+    
+    IEnumerator SpawnWave(GameObject prefab, int numberOfEnemies, float delayBetweenSpawns)
+    {        
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            yield return new WaitForSeconds(delayBetweenSpawns);
+            Instantiate(prefab, RandomPosition(), Quaternion.identity);
+        }
+    }
+    IEnumerator SpawnWave(GameObject prefab, int numberOfEnemies, float delayBetweenSpawns, 
+                          GameObject prefabTwo, int numberOfEnemiesTwo, float delayBetweenSpawns2)
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            yield return new WaitForSeconds(delayBetweenSpawns);
+            Instantiate(prefab, RandomPosition(), Quaternion.identity);
+        }
+        for (int i = 0; i < numberOfEnemiesTwo; i++)
+        {
+            yield return new WaitForSeconds(delayBetweenSpawns2);
+            Instantiate(prefabTwo, RandomPosition(), Quaternion.identity);
+        }
     }
 }
