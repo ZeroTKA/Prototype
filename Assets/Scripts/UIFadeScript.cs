33d1ffb88c5cb246f618e7f37e2b4ebd1913d0db
ZeroTKA <zerotka@gmail.com>
2025-10-03 20:56:51 -0600

    Did a Code Review of UIFadeScript
    Not too much going on. Added null checks and error logging.

--- Assets/Scripts/UIFadeScript.cs
+++ Assets/Scripts/UIFadeScript.cs
@@ -5,14 +5,31 @@ public class UIFadeScript : MonoBehaviour
 {
     [SerializeField] private CanvasGroup canvasGroup;
 
+    private void Start()
+    {
+        if (canvasGroup == null)
+        {
+            Debug.LogError("[UIFadeScript] canvasGroup is null. It'll break for sure.");
+        }
+    }
     public void FadeToBlack(float duration = 1.7f)
     {
+        if (canvasGroup == null)
+        {
+            Debug.LogError("[UIFadeScript] canvasGroup is null. Can't Fade to black.");
+            return;
+        }
         canvasGroup.DOFade(1f, duration).SetUpdate(true);
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = true;
     }
     public void FadeBlackToClear(float duration = 1.7f)
     {
+        if (canvasGroup == null)
+        {
+            Debug.LogError("[UIFadeScript] canvasGroup is null. Can't fade black to clear.");
+            return;
+        }
         canvasGroup.DOFade(0f, duration).SetUpdate(true);
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
