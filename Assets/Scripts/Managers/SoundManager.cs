efe3e8d0e024c093f2c0c0314abc3c6cc20c15b6
ZeroTKA <zerotka@gmail.com>
2025-10-20 15:15:26 -0600

    Setting up public enums.
    Need to convert everything to the poolmanager retrieval.

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Reflection;
 
 public class SoundManager : MonoBehaviour
 {
@@ -25,9 +26,93 @@ public class SoundManager : MonoBehaviour
             Debug.LogWarning("Sound Manager already exists. Destroying this");
         }
     }
-        
-    void Update()
+    private void Start()
     {
-        
+        CheckSounds();
+        m_MusicSource.PlayOneShot(rainLoop);
+    }
+
+    public void PlaySound(SoundType type, Vector3 position)
+    {
+        AudioClip clip = null;
+        switch(type)
+        {
+            case SoundType.Gunshot:
+                clip = gunshots;
+                break;
+            case SoundType.ReloadingBegin:
+                clip = reloading[0];
+                break;
+            case SoundType.ReloadingMid:
+                clip = reloading[1];
+                break;
+            case SoundType.ReloadingEnd:
+                clip = reloading[2];
+                break;
+            case SoundType.Rain:
+                clip = rainLoop;
+                break;
+            case SoundType.Fire:
+                clip = fireLoop;
+                break;
+            case SoundType.Gravel:
+                clip = gravelWalking[Random.Range(0, gravelWalking.Length)];
+                break;
+            case SoundType.Stone:
+                clip = stoneWalking[Random.Range(0,stoneWalking.Length)];
+                break;
+        }
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, position);
+        }
+    }
+    private void CheckSounds()
+    {
+        var fields = typeof(SoundManager).GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        foreach (var field in fields)
+        {
+            // -- Single clips -- //
+            if (field.FieldType == typeof(AudioClip))
+            {
+                var clip = field.GetValue(this) as AudioClip;
+                if (clip == null)
+                {
+                    Debug.LogWarning($"[SoundManager] '{field.Name}' is null.");
+                }
+            }
+
+            // -- Audio clip lists -- //
+            else if (field.FieldType == typeof(AudioClip[]))
+            {
+                var clips = field.GetValue(this) as AudioClip[];
+                if (clips != null)
+                {
+                    for (int i = 0; i < clips.Length; i++)
+                    {
+                        if (clips[i] == null)
+                        {
+                            Debug.LogWarning($"[SoundManager] '{field.Name}' is null at index {i}.");
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"[SoundManager] AudioClip array '{field.Name}' is null.");
+                }
+            }
+        }
+
+    }
+    public enum SoundType
+    {
+        Gunshot,
+        ReloadingBegin,
+        ReloadingMid,
+        ReloadingEnd,
+        Rain,
+        Fire,
+        Gravel,
+        Stone
     }
 }
